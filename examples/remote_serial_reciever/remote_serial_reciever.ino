fc61b531d7e69876f1c943d4f282d64ce14f4a3d
BlackCommando2 <keshavsoni2002@gmail.com>
2023-04-01 15:01:46 +0530

    Updated Example Code

--- examples/remote_serial_reciever/remote_serial_reciever.ino
+++ examples/remote_serial_reciever/remote_serial_reciever.ino
@@ -8,21 +8,20 @@ void setup()
 {
   Serial.begin(115200);
   Serial2.begin(921600, SERIAL_8N1, 16, 17);
-  registerSerial(&Serial2);
+  registerSerial(&Serial2); // register which Serial to Listen at
   setId("TenZZ");
   baseDirection.init("WardL");
   Serial2.setTimeout(1);
-  baseDirection.setOnRecieve(defaultHandler);
-  setSerialReciever(cross, "cross");
+  baseDirection.setOnRecieve(defaultHandler); // set function to send remote analog Value to Base
+  setSerialReciever(cross, "cross"); // set funciton for particular string recieved
   setSerialReciever(distanceModeOn, "dmOn");
   setSerialReciever(distanceModeOff, "dmOff");
   setBaseReciever(sendBase, "base");
-  
 }
 
 void loop()
 {
-  serialListener();
+  serialListener(); //to start serial listener
 }
 
 void cross(String msg)
